4ae680655a25c222f2c8533c982a13c3a845426f
Alan Smith <alan@randomsmiths.com>
2025-12-10 15:46:19 -0500

    Part 2

--- day07/main.py
+++ day07/main.py
@@ -53,6 +53,70 @@ def beam(s_position: tuple[int, int], carets: dict, max_row: int, max_col: int) 
     return split_count
 
 
+def beam2(s_position: tuple[int, int], carets: dict, max_row: int, max_col: int) -> int:
+    """
+    Simulates a beam starting from S position going down.
+    When it hits a '^', it can choose to go either left OR right (not both).
+    Returns the total number of possible paths.
+    """
+    if s_position is None:
+        return 0
+    
+    # Memoization cache: (row, col) -> number of paths from that position
+    memo = {}
+    # Set to track positions currently being computed (to detect cycles)
+    computing = set()
+    
+    def count_paths(row: int, col: int) -> int:
+        # Check if out of bounds
+        if row >= max_row or col < 0 or col >= max_col:
+            return 1  # One path: going out of bounds
+        
+        # Check memoization
+        if (row, col) in memo:
+            return memo[(row, col)]
+        
+        # Check for cycle (position being computed)
+        if (row, col) in computing:
+            return 0  # Cycle detected, no paths from this cycle
+        
+        # Mark as computing
+        computing.add((row, col))
+        
+        try:
+            # Check if we hit a '^'
+            if (row, col) in carets:
+                # Can choose either left OR right
+                total_paths = 0
+                
+                # Option 1: Go left - move to (row, col-1), then continue from there
+                left_col = col - 1
+                if left_col >= 0:
+                    # Move horizontally to left, then continue going down
+                    # We process the left position (which might be a '^' or not), then go down
+                    total_paths += count_paths(row, left_col)
+                
+                # Option 2: Go right - move to (row, col+1), then continue from there
+                right_col = col + 1
+                if right_col < max_col:
+                    # Move horizontally to right, then continue going down
+                    # We process the right position (which might be a '^' or not), then go down
+                    total_paths += count_paths(row, right_col)
+                
+                memo[(row, col)] = total_paths
+                return total_paths
+            else:
+                # Continue going down
+                result = count_paths(row + 1, col)
+                memo[(row, col)] = result
+                return result
+        finally:
+            # Remove from computing set
+            computing.remove((row, col))
+    
+    return count_paths(s_position[0], s_position[1])
+
+
 def main():
     parser = argparse.ArgumentParser(description='Parse grid file for S and ^ characters')
     parser.add_argument('file', type=str, help='Path to the input file')
@@ -81,6 +145,10 @@ def main():
     # Call the beam method
     splits = beam(s_position, carets, max_row, max_col)
     print(f"Number of splits: {splits}")
+    
+    # Call the beam2 method
+    total_paths = beam2(s_position, carets, max_row, max_col)
+    print(f"Total number of possible paths: {total_paths}")
     
     return s_position, carets
 
